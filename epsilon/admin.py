c58f6b5b789f851dabb888b3180af73e8dcccb1c
Nimitha jagadeesha <nimitha1jagadeesha@gmail.com>
2022-03-15 20:03:25 +0530

    admi.py

--- epsilon/admin.py
+++ epsilon/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Question,Score,Display,Profile
+from .models import Question, Score, Display, Profile, Lifeline
 # Register your models here.
 admin.site.register(Question)
 admin.site.register(Score)
 admin.site.register(Display)
 admin.site.register(Profile)
+admin.site.register(Lifeline)
